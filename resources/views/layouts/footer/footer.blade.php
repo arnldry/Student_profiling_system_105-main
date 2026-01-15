f89f38a6af4d7add482f940a55a534a816693af6
john <renajohncarlo0@gmail.com>
2026-01-16 02:13:25 +0800

    Revert "Merge branch 'main' of https://github.com/arnldry/Student_profiling_system_105-main"
    This reverts commit e8e38b964716fa319207ad97d34697e6a42e7deb, reversing
    changes made to 9b3cdadec5af34c3c3fae4b406ac886b99813ee3.

--- resources/views/layouts/footer/footer.blade.php
+++
@@ -1,9 +0,0 @@
-
-    <div class="container mx-auto flex justify-between items-center px-4">
-        <div class="text-sm">
-            OCNHS Guidance Department &mdash; Student Profiling System
-        </div>
-        <div class="text-sm">
-            &copy; {{ date('Y') }} OCNHS. All Rights Reserved.
-        </div>
-    </div>
